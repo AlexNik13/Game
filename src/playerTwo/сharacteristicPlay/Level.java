9a4551e9ef873e1ee8dcda56b29230e5a12695a0
AlexNik13 <4pnav21@gmail.com>
2021-11-08 17:38:31 +0200

    two player add level up

--- src/playerTwo/сharacteristicPlay/Level.java
+++ src/playerTwo/сharacteristicPlay/Level.java
@@ -1,13 +1,36 @@
 package playerTwo.сharacteristicPlay;
 
+import common.NewLevel;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Level {
     private int level;
     private int experience;
     private int freeStatPoints;
+    public static Map<Integer, Integer> experienceLevelUp = new HashMap<>();
 
+    static {
+        experienceLevelUp.put(1, 9);
+        experienceLevelUp.put(2, 19);
+        experienceLevelUp.put(3, 49);
+        experienceLevelUp.put(4, 99);
+        experienceLevelUp.put(5, 149);
+        experienceLevelUp.put(6, 199);
+        experienceLevelUp.put(7, 250);
+        experienceLevelUp.put(8, 350);
+    }
 
     public Level() {
         this.level = 1;
+    }
+
+    private void checkLevelUp() {
+        if(experience > experienceLevelUp.get(level)){
+            System.out.println("\nLEVEL UP\n");
+            levelUp();
+        }
     }
 
     public int getLevel() {
@@ -16,10 +39,12 @@ public class Level {
 
     public void levelUp(){
         this.freeStatPoints += 3;
+        level++;
     }
 
     public void addExperience(int experience) {
         this.experience += experience;
+        checkLevelUp();
     }
 
     public int getExperience() {
